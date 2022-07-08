cb6de521060e8f8edc83fc649629307d061226e0
halimatsadiat <66258094+halimatsadiat@users.noreply.github.com>
2022-07-08 17:40:37 +0100

    added routes

---
+++ routes/todo.js
@@ -0,0 +1,28 @@
+//const Todo = require("../models/Todo");
+
+const router = require("express").Router()
+const Todo = require("../models/Todo")
+
+router.post("/add/todo", (req, res) => {
+    const {todo} = req.body;
+    const newTodo = new Todo({ todo });
+
+    newTodo.save()
+    .then(()=> {
+        console.log("Successfully addedd todo")
+        res.redirect("/")
+    })
+    .catch((err) => console.log(err));
+})
+
+.get("/delete/todo/:_id", (req, res) => {
+    const {_id} = req.params;
+    Todo.deleteOne({_id})
+    .then(() => {
+        console.log("Deleted Successfully")
+        res.redirect("/")
+    })
+    .catch((err) => console.log(err));
+})
+
+module.exports = router
